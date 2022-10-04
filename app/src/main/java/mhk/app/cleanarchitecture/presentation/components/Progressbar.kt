a1330ff3294473fb8543e4e8864c9b002da8b8ba
Humza <alfaios123@gmail.com>
2022-10-04 21:28:48 +0500

    Initial Commit

---
+++ app/src/main/java/mhk/app/cleanarchitecture/presentation/components/Progressbar.kt
@@ -0,0 +1,20 @@
+package mhk.app.cleanarchitecture.presentation.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun ProgressBar() {
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
